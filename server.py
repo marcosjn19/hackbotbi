f93951737e3b5b08f414a7772284bf9fc56d7f36
marcosjn19 <marcosjn719@gmail.com>
2025-01-11 19:21:30 -0600

    Fix: Heroku routes

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(
     __name__,
-    static_folder=os.path.join(BASE_DIR, '../hackbotbi-web/dist/static'),
-    template_folder=os.path.join(BASE_DIR, '../hackbotbi-web/dist')
+    static_folder=os.path.join(BASE_DIR, './hackbotbi-web/dist/static'),
+    template_folder=os.path.join(BASE_DIR, './hackbotbi-web/dist')
 )
 
 cors = CORS( app, origins = '*', supports_credentials=True)
